5aa57439872d287102fc045abe84296db45a1f51
itza2k <161753369+itza2k@users.noreply.github.com>
2024-09-21 15:07:20 +0530

    as a screen

---
+++ app/src/main/java/com/teama/hopeline/ui/features/LoginScreen.kt
@@ -0,0 +1,73 @@
+package com.teama.hopeline.ui.features
+
+import android.content.Context
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.teama.hopeline.ui.theme.HopeLineTheme
+
+@Composable
+fun LoginScreen() {
+    val context = LocalContext.current
+    val googleSignInClient = remember { getGoogleSignInClient(context) }
+    val signInLauncher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+        handleSignInResult(task)
+    }
+
+    LaunchedEffect(Unit) {
+        signInLauncher.launch(googleSignInClient.signInIntent)
+    }
+
+    HopeLineTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
+
+private fun getGoogleSignInClient(context: Context): GoogleSignInClient {
+    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestEmail()
+        .build()
+    return GoogleSignIn.getClient(context, gso)
+}
+
+private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+    try {
+        val account = completedTask.getResult(ApiException::class.java)
+        // Signed in successfully, show authenticated UI.
+        // You can now use the account object to access user information
+    } catch (e: ApiException) {
+        // Sign in was unsuccessful, handle the error
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HopeLineTheme {
+        Greeting("Android")
+    }
+}
