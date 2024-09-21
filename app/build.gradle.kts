66a627b87beb511db150963d24c6650a5a3ceeb8
itza2k <161753369+itza2k@users.noreply.github.com>
2024-09-21 15:15:01 +0530

    retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:20.0.0")
 
     implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
--- app/src/main/java/com/teama/hopeline/data/ApiService.kt
+++ app/src/main/java/com/teama/hopeline/data/ApiService.kt
@@ -1,4 +1,10 @@
 package com.teama.hopeline.data
 
-class ApiService {
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+    @GET("endpoint/{id}")
+    fun getData(@Path("id") id: String): Call<YourDataModel>
 }
